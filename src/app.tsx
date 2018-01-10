b8cebc4ebcf4c5d551894fd54156415a49e02df8
Fernando Mendoza <goliat8903@gmail.com>
2018-01-09 21:04:27 -0600

    Create App stateless component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello React</div>, document.getElementById('root'));
+type AppProps = { message: string };
+const App: React.SFC<AppProps> = ({ message }) => <div>{message}</div>;
+
+ReactDOM.render(<App message="Hellow world"/>, document.getElementById('root'));
